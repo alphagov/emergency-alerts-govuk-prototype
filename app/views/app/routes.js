cba78ac8a46faa002b5dfc6f2efb32ac8881b03a
cabinet-office-ea <120734372+cabinet-office-ea@users.noreply.github.com>
2023-07-10 11:38:55 +0100

    app

---
+++ app/views/app/routes.js
@@ -0,0 +1,30 @@
+//
+// For guidance on how to create routes see:
+// https://prototype-kit.service.gov.uk/docs/create-routes
+//
+
+const govukPrototypeKit = require('govuk-prototype-kit')
+const router = govukPrototypeKit.requests.setupRouter()
+
+// Add your routes here
+
+// Run this code when a form is submitted to 'juggling-balls-answer'
+router.post('/juggling-balls-answer', function (req, res) {
+
+  // Make a variable and give it the value from 'how-many-balls'
+  var howManyBalls = req.session.data['how-many-balls']
+
+  // Check whether the variable matches a condition
+  if (howManyBalls == "3 or more"){
+    // Send user to next page
+    res.redirect('/page1')
+} 
+  if (howManyBalls == "1 or 2"){
+ // Send user to next page
+ res.redirect('/page2')
+}
+if (howManyBalls == "None - I cannot juggle"){
+    // Send user to next page
+    res.redirect('/page3')
+   }    
+})
